4294e794c51f1345822b70b9cada33bbc1194d17
patrikslore-lab <patriks.lore@gmail.com>
2025-12-08 19:16:24 +0000

    08 12
    Rerfactoring Ability Mgr

--- Assets/2. Scripts/2. Controllers/AbilityController.cs
+++ Assets/2. Scripts/2. Controllers/AbilityController.cs
@@ -1,16 +1,67 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AbilityController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+{   private static AbilityController instance;
+    public static AbilityController Instance => instance;
+    private PlayerConfig playerConfig;
+
+    [SerializeField] RicochetAbility ricochetController;
+    [SerializeField] RicochetAbility protectorController;
+    [SerializeField] RicochetAbility looterController;
+    [SerializeField] BaseAbility baseAbilityController;
+
+    private float cooldownTimer = 0f;
+    private bool isOnCooldown = false;
+    public bool IsOnCooldown => isOnCooldown;
+    public float CooldownRemaining => isOnCooldown ? (playerConfig.juneCooldown - cooldownTimer) : 0f;
+    public float CooldownProgress => isOnCooldown ? (cooldownTimer / playerConfig.juneCooldown) : 0f;
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerConfig = GameManager.Instance.GetPlayerConfig();
+    }
+
+    private void Update()
+    {
+        if (isOnCooldown)
+        {
+            cooldownTimer += Time.deltaTime;
+
+            if (cooldownTimer >= playerConfig.juneCooldown)
+            {
+                EndGlobalCooldown();
+            }
+        }
+    }
+
+    //------------------------------------------------------
+    // COOLDOWN CONTROLS
+    //------------------------------------------------------
+    public void StartGlobalCooldown()
+    {
+        isOnCooldown = true;
+        cooldownTimer = 0f;
+        Debug.Log($"Global ability cooldown started: {playerConfig.juneCooldown}s");
+    }
+
+    private void EndGlobalCooldown()
+    {
+        isOnCooldown = false;
+        cooldownTimer = 0f;
+        UIManager.Instance.RicochetAvailable();
+        UIManager.Instance.LooterAvailable();
+        UIManager.Instance.ProtectorAvailable();
+        Debug.Log("Global ability cooldown complete - ALL abilities available");
     }
 }
